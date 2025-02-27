8ecc8168e4e48a24ecf5808a18d4ea587a95612c
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-02-28 00:33:18 +0530

    update stack navigations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,17 @@
 import {Stack} from "expo-router";
-
+import AddNote from "./AddNote";
+import TabLayout from "./(tabs)/_layout";
+import {Provider} from "react-redux";
+import store from "./store";
 export default function Layout() {
     return (
+        <Provider store={store}>
         <Stack>
             <Stack.Screen name="index" options={{headerShown: false}}/>
             <Stack.Screen name="signup" options={{headerShown: false}}/>
-            <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+            <Stack.Screen name="(tabs)"  options={{ headerShown: false }} />
+            <Stack.Screen name="AddNote" options={{ headerShown: false }} />
         </Stack>
+        </Provider>
     );
 }
